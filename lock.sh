bf6e2df69e8b90d62ba91f9c8dcaae00c05cc4c8
sawsuh <prao060@gmail.com>
2018-10-05 12:07:39 +1000

    using fancier i3lock config

--- lock.sh
+++ lock.sh
@@ -4,9 +4,9 @@ scrot /home/prashant/.screen.png
 icon="/$HOME/.lock.png"
 tmpbg="$HOME/.screen.png"
 
-(( $# )) && { icon=$1; }
-
+#(( $# )) && { icon=$1; }
+convert "$tmpbg" -colorspace Gray "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -u -i "$tmpbg"
+#convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+i3lock 
 rm "$tmpbg"
