0e7a9265c3ca2fb89fde77a2ace3d7ce87fc36de
sawsuh <prao060@gmail.com>
2018-10-06 12:37:27 +1000

    fixed up lockscreen brightness to ensure text visibility

--- lock.sh
+++ lock.sh
@@ -6,6 +6,7 @@ tmpbg="$HOME/.screen.png"
 
 #(( $# )) && { icon=$1; }
 convert "$tmpbg" -colorspace Gray "$tmpbg"
+convert "$tmpbg" -brightness-contrast -50x0 "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 #convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 i3lock 
