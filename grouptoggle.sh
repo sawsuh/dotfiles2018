f65bac9e3ae9eb0fe2ab97371c1409ae7c1a0ad7
sawsuh <prao060@gmail.com>
2018-11-09 23:52:54 +1100

    fixed groups a lil

--- grouptoggle.sh
+++ grouptoggle.sh
@@ -1,3 +1,7 @@
-waitron group_toggle $1
-k=$(bash ~/.scripts/numtoword.sh $1)
-dunstify -r 1 $k
+if [ $2 = "show" ]; then
+	waitron group_activate $1
+	k=$(bash ~/.scripts/numtoword.sh $1)
+	dunstify -r 1 $k
+else
+	waitron group_deactivate $1
+fi
