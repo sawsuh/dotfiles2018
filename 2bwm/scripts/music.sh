c20f167da431d67add9a5841bfb1e1b53c20e7a6
sawsuh <prao060@gmail.com>
2018-11-14 20:15:23 +1100

    sticky windows

--- 2bwm/scripts/music.sh
+++ 2bwm/scripts/music.sh
@@ -1,9 +1,8 @@
-tdrop -a -h 309 -w 721 -x 10 -y 751 -n 1 -f "-name music -e ncmpcpp" urxvt
+tdrop -a -h 309 -w 721 -x 10 -y 751 -n 2 -f "-name music -e ncmpcpp" urxvt
 if wmctrl -l | grep ncmpcpp; then
 	k=$(xdotool search --classname "music")
 	xdotool windowraise $k
-	xdotool windowfocus $k
-	sleep 0.1
+	xdotool windowfocus $k 
 	xdotool mousemove 50 800
 	sleep 0.1
 	xdotool key "Super+f"
