d8767c258a57db91527730400c3f916c32d9a5fa
sawsuh <prao060@gmail.com>
2019-02-06 16:21:41 +1100

    made calendar work for all months

---
+++ 2bwm/scripts/cal.sh
@@ -0,0 +1,12 @@
+k=$(xdotool search --classname "cal")
+hex=$(printf '%x\n' $k)
+if [ $hex == "0" ]; then
+	urxvt -g 20x10 -name cal -hold -e zsh -c 'cal && setterm -cursor off'& 
+	sleep 0.1
+	xdotool key "Super+t"
+	xdotool key "Super+j"
+else
+	xkill -id $k;
+fi
+
+
