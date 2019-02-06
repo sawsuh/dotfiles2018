8eea64b43d02013b3cae54a383cf944bcb33f0a8
sawsuh <prao060@gmail.com>
2019-02-06 16:17:06 +1100

    made launcher on top always

--- 2bwm/scripts/launcher.sh
+++ 2bwm/scripts/launcher.sh
@@ -1,4 +1,5 @@
 urxvt -g 90x1+595+479 -e bash -c 'LAUNCHER=true /bin/zsh -i -t'& > /dev/null
 sleep 0.1
 xdotool mousemove 1100 540
-xdotool key "Super+j" 
+xdotool key "Super+t"
+xdotool key "Super+j"
