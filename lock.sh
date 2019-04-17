4992273bf200ec127ca28a8e007e9efc1fad02c0
sawsuh <prao060@gmail.com>
2019-04-17 15:45:44 +1000

    fully switched to bspwm dual monitor

--- lock.sh
+++ lock.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-scrot /home/prashant/.screen.png
-icon="/$HOME/.login.png"
-tmpbg="$HOME/.screen.png"
+#scrot /home/prashant/.screen.png
+#icon="/$HOME/.login.png"
+#tmpbg="$HOME/.screen.png"
 
 #(( $# )) && { icon=$1; }
 #convert "$tmpbg" -colorspace Gray "$tmpbg"
 #convert "$tmpbg" -brightness-contrast -40x0 "$tmpbg"
 #convert "$tmpbg" -scale 20% -scale 500% "$tmpbg"
 #convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-convert "$tmpbg" -blur 0x4 "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+#convert "$tmpbg" -blur 0x4 "$tmpbg"
+#convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 i3lock 
-rm "$tmpbg"
+#rm "$tmpbg"
--- scripts/cal.sh
+++ scripts/cal.sh
@@ -1,9 +1,7 @@
 k=$(xdotool search --classname "cal")
 hex=$(printf '%x\n' $k)
 if [ $hex == "0" ]; then
-	urxvt -g 20x10 -name cal -hold -e zsh -c 'cal && setterm -cursor off'& 
-	sleep 0.1
-	xdotool key "Super+j"
+	urxvt -g 20x10+1580+65 -name cal -hold -e zsh -c 'cal && setterm -cursor off'& 
 else
 	xkill -id $k;
 fi
