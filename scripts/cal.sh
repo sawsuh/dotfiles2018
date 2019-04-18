5ff091e4fc14f9867c4c569a41bbcc58f976fdc3
sawsuh <prao060@gmail.com>
2019-04-18 19:55:38 +1000

    more dual monitor changes

--- scripts/cal.sh
+++ scripts/cal.sh
@@ -1,7 +1,7 @@
 k=$(xdotool search --classname "cal")
 hex=$(printf '%x\n' $k)
 if [ $hex == "0" ]; then
-	urxvt -g 20x10+1580+65 -name cal -hold -e zsh -c 'cal && setterm -cursor off'& 
+	urxvt -g 20x10+2964+2058 -name cal -hold -e zsh -c 'cal && setterm -cursor off'& 
 else
 	xkill -id $k;
 fi
