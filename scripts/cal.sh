ef85786a3da2eeb8a3417c41e33369fbed73612b
sawsuh <prao060@gmail.com>
2019-04-19 02:04:59 +1000

    changed wallpapers to a bit of a spacey theme

--- scripts/cal.sh
+++ scripts/cal.sh
@@ -1,7 +1,7 @@
 k=$(xdotool search --classname "cal")
 hex=$(printf '%x\n' $k)
 if [ $hex == "0" ]; then
-	urxvt -g 20x10+2964+2058 -name cal -hold -e zsh -c 'cal && setterm -cursor off'& 
+	urxvt -g 20x10+2984+2079 -name cal -hold -e zsh -c 'cal && setterm -cursor off'& 
 else
 	xkill -id $k;
 fi
