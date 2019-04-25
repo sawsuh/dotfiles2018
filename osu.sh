971078e105fe3872ae8aefc7e050f791da6611d2
sawsuh <prao060@gmail.com>
2019-04-25 13:23:01 +1000

    hmmmm

--- osu.sh
+++ osu.sh
@@ -5,6 +5,6 @@ xrandr --output HDMI-1 --off
 xsetwacom set 9 MapToOutpot 1920x1080+0+0
 cd ~/.osu && dotnet run --project osu.Desktop -c Release
 pkill pavucontrol
-xrandr --output DVI-I-1 --off --output HDMI-1 --mode 2560x1440 --pos 1920x0 1x1 --output DP-1 --off --output DVI-D-1 --primary --mode 1920x1080 --rate 144
+xrandr --output HDMI-1 --right-of DVI-D-1 --mode 2560x1440 --pos 1920x0 --rotate normal --output DVI-D-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --rate 144
 xsetwacom set 9 MapToOutpot 1920x1080+0+0
 
