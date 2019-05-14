e6e9690a3ce77671ffb35e98928e0d8b727fa089
sawsuh <prao060@gmail.com>
2019-05-14 12:42:51 +1000

    made polybar flat again, added music and mpd

--- scripts/cal.sh
+++ scripts/cal.sh
@@ -1,7 +1,7 @@
 k=$(xdotool search --classname "cal")
 hex=$(printf '%x\n' $k)
 if [ $hex == "0" ]; then
-	urxvt -g 20x10+4144+934 -name cal -hold -e zsh -c 'cal && setterm -cursor off'& 
+	urxvt -g 20x10+4144+949 -name cal -hold -e zsh -c 'cal && setterm -cursor off'& 
 else
 	xkill -id $k;
 fi
