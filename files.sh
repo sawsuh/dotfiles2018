b645659313923a0ec8e7fde094b4e685be2f9392
sawsuh <prao060@gmail.com>
2019-10-31 23:10:56 +1100

    made borders 5 pixels instead of 3

--- files.sh
+++ files.sh
@@ -1,7 +1,7 @@
 xcord=$(xdotool getmouselocation --shell | awk -F '=' '{print $2; exit}')
 if [[ "$xcord" > 2560 ]]
 then
-	tdrop -a -h 600 -w 1200 -x 3904 -y 824 -n 1 -f "-name stick1 -e ranger" urxvt	
+	tdrop -a -h 600 -w 1200 -x 3900 -y 820 -n 1 -f "-name stick1 -e ranger" urxvt	
 else
-	tdrop -a -h 600 -w 1200 -x 1344 -y 824 -n 1 -f "-name stick1 -e ranger" urxvt
+	tdrop -a -h 600 -w 1200 -x 1340 -y 820 -n 1 -f "-name stick1 -e ranger" urxvt
 fi 
