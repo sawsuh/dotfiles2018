e91b2bcdfe42da9603d492ca28c04b8fe5a9e2a0
sawsuh <prao060@gmail.com>
2019-12-08 20:22:45 +1100

    updated scripts to use functions, making them more readable

--- files.sh
+++ files.sh
@@ -1,21 +1,28 @@
+function tdropfunc {
+	tdrop -a -h 600 -w 1200 -x $1 -y $2 -n 1 -f "-name stick1 -e ranger" urxvt
+}
+
+function bartest {	
+	pid=$(pgrep -a polybar | grep $1 | awk '{print $1}')
+	xdotool search --pid $pid --onlyvisible
+}
+
 xcord=$(xdotool getmouselocation --shell | awk -F '=' '{print $2; exit}')
 if [[ "$xcord" > 2560 ]]
 then 
-	pid=$(pgrep -a polybar | grep pop2 | awk '{print $1}')
-	wid=$(xdotool search --pid $pid --onlyvisible)
+	wid=$(bartest pop2)
 	if [[ "$wid" != "" ]]
 	then
-		tdrop -a -h 600 -w 1200 -x 3902 -y 752 -n 1 -f "-name stick1 -e ranger" urxvt
+		tdropfunc 3902 752
 	else
-		tdrop -a -h 600 -w 1200 -x 3902 -y 822 -n 1 -f "-name stick1 -e ranger" urxvt
+		tdropfunc 3902 822
 	fi
 else
-	pid=$(pgrep -a polybar | grep pop1 | awk '{print $1}')
-	wid=$(xdotool search --pid $pid --onlyvisible)
+	wid=$(bartest pop1)
 	if [[ "$wid" != "" ]]
 	then
-		tdrop -a -h 600 -w 1200 -x 1342 -y 752 -n 1 -f "-name stick1 -e ranger" urxvt
+		tdropfunc 1342 752
 	else
-		tdrop -a -h 600 -w 1200 -x 1342 -y 822 -n 1 -f "-name stick1 -e ranger" urxvt
+		tdropfunc 1342 822
 	fi
 fi 
