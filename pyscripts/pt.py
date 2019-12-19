51edd79310bcae6dcfaab417d1bde024a7e73d7c
sawsuh <prao060@gmail.com>
2019-12-20 01:19:39 +1100

    updated pt script

--- pyscripts/pt.py
+++ pyscripts/pt.py
@@ -16,13 +16,13 @@ recordcmd = [
 recording = sp.run(recordcmd, stdout=sp.PIPE, stderr=sp.DEVNULL)
 width, height, topx, topy = recording.stdout.decode("utf-8").split(" ")
 
-width = int(width) + 8
-height = int(height) + 8
-topx = int(topx) - 4
-topy = int(topy) - 4
+width = int(width) - 8
+height = int(height) - 8
+topx = int(topx) + 4
+topy = int(topy) + 4
 
 sp.Popen(["urxvt", "-name", "float"])
-time.sleep(0.100)
+time.sleep(0.150)
 
 widcommand = sp.run(["xdotool", "getwindowfocus"],
                     stdout=sp.PIPE, stderr=sp.DEVNULL)
@@ -30,3 +30,7 @@ wid = hex(int(widcommand.stdout.decode("utf-8")))
 geomstring = f"0,{topx},{topy},{width},{height}"
 resizecmd = ["wmctrl", "-i", "-r", wid, "-e", geomstring]
 sp.run(resizecmd)
+middlex = topx + width / 2
+middley = topy + height / 2
+sp.run(["xdotool", "mousemove", str(middlex), str(middley)])
+sp.run(["xdotool", "windowactivate", wid])
